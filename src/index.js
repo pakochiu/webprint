28894b0a0cd039462ddfacb36a9a40e0ccc8860e
Pako Chiu 趙少輝 <pako.chiu@iisigroup.com>
2022-05-18 11:37:53 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// ./src/index.js
+const benchmarker = (testFunction, times = 1000000) => {
+    if (typeof testFunction !== 'function') {
+      throw new Error('Did not provide a valid function for test.');
+    }
+  
+    const startTime = new Date().getTime();
+  
+    let i = 0;
+    while (i < times) {
+      i++;
+      testFunction();
+    }
+  
+    const endTime = new Date().getTime();
+  
+    return endTime - startTime;
+  };
+  
+  // 匯出函式
+  module.exports = benchmarker;
